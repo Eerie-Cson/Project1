b2bdff1906421441e20bab92740be3f5831271f6
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-08-28 14:31:48 +0800

    Feature test connectivity (#3)
    * feat(api): install supertest and jest package
    
    * refactor(api): export server init module
    
    * test(api): test server status
    
    * style(api): add new line at the end of the line

---
+++ server/tests/index.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest'); 
+const app = require('../index');
+
+describe('server response', ()=>{
+  const server = app.listen(8000);
+  afterAll(() => {
+    server.close();
+  });
+
+  it('should give a response of 200', async ()=>{
+    const response = await request(server).get('/');
+    expect(response.status).toBe(200);
+  })
+});
