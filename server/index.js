bdca031c279446ebab0a548ed758db8a994a3d96
Ericson Sacdalan <ericson.sacdalan.4@gmail.com>
2022-08-28 17:43:03 +0800

    feat(api): add a db connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const Koa = require('koa');
+const Mongoose = require('mongoose');
 const rootRouter = require('./routes');
 
+Mongoose.connect('mongodb://localhost:27017/Category');
+
 const app = new Koa();
-app.use(rootRouter.routes());
-app.use(rootRouter.allowedMethods());
+app.use(rootRouter.routes()).use(rootRouter.allowedMethods());
 
 module.exports = app;
