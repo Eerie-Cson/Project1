598cf81b5e7f128ae465feed52742f64b02046fd
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-09-07 13:34:55 +0800

    feat(api): install body-parser (#10)
    * feat(api): install body-parser
    
    * feat(api): update body parser

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const Koa = require('koa');
-const Mongoose = require('mongoose');
 const rootRouter = require('./routes');
+const bodyParser = require('koa-bodyparser');
+const Mongoose = require('mongoose');
 
 Mongoose.connect('mongodb://localhost:27017/Category');
 
 const app = new Koa();
+app.use(bodyParser());
 app.use(rootRouter.routes()).use(rootRouter.allowedMethods());
 
 module.exports = app;
