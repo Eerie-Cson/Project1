33d6bf54e3c69c5ebf43f17b1a286f1ff43c22dd
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-09-13 19:54:25 +0800

    refactor(api): deconstruct module exporting

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,9 @@
 const items = require('../model');
-module.exports = {
+class Items {
   async createItem({item, stock, price}){ 
     const item = await items.create({item, stock, price});
     return item;
-  },
+  };
 };
+
+module.exports = Items;
