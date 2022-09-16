98ae5d8bb373d696fa071057002df2057a80145e
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-09-16 09:16:48 +0800

    style(api): remove console.log

--- server/api/services/tests/index.test.js
+++ server/api/services/tests/index.test.js
@@ -11,15 +11,17 @@ afterEach(() => {
 describe('ItemServices', () => {
   describe('#createDepartment', () => {
     it('should create new department', async() => {
+      
       const data = {
         item: chance.word(),
         stock: chance.natural(),
         price: chance.floating({fixed: 2}),
       }
+
       Item.create = jest.fn().mockImplementation(() => data);
       
       const result = await item.createItem(data);
-      console.log(result);
+
       expect(result).toBeDefined();
       expect(Item.create).toBeCalled();
       expect(result).toStrictEqual(data);
