c34f6094e1454527d452c339efe30b1038a5868e
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-09-16 09:19:37 +0800

    test(api): mock createItem function in ItemServices (#26)
    * test(api): mock createItem function in ItemServices
    
    * style(api): remove console.log

---
+++ server/api/services/tests/index.test.js
@@ -0,0 +1,30 @@
+const Item = require('../../model/item');
+const ItemServices = require('../items');
+const { Chance } = require('chance');
+
+const chance = new Chance();
+const item = new ItemServices();
+
+afterEach(() => {
+  jest.clearAllMocks();
+})
+describe('ItemServices', () => {
+  describe('#createDepartment', () => {
+    it('should create new department', async() => {
+      
+      const data = {
+        item: chance.word(),
+        stock: chance.natural(),
+        price: chance.floating({fixed: 2}),
+      }
+
+      Item.create = jest.fn().mockImplementation(() => data);
+      
+      const result = await item.createItem(data);
+
+      expect(result).toBeDefined();
+      expect(Item.create).toBeCalled();
+      expect(result).toStrictEqual(data);
+    })
+  })
+})
--- server/server.js
+++ server/server.js
@@ -2,12 +2,11 @@ const Koa = require('koa');
 const rootRouter = require('./api/routes');
 const bodyParser = require('koa-bodyparser');
 const mongoose = require('mongoose');
-
 mongoose.connect('mongodb://localhost:27017/inventoref');
 
 const app = new Koa();
 app.use(bodyParser());
 app.use(rootRouter.routes()).use(rootRouter.allowedMethods());
 
-app.listen(3000);
+// app.listen(3000);
 module.exports = app;
