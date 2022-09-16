9c4d709b4b175a65471a27041c68c7e58c8b4715
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-09-16 18:11:45 +0800

    refactor(api): change the details of item schema (#27)

--- server/api/model/item.js
+++ server/api/model/item.js
@@ -1,10 +1,27 @@
+const { Chance } = require('chance');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const chance = new Chance()
+
 const itemSchema = new Schema({
-  item: String,
-  stock: Number,
-  price: Number
+  _id: {
+    type: String,
+    unique: true,
+    default: chance.String()
+  },
+  item: {
+    type: String,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    default: 0
+  },
+  price:{ 
+    type: Number,
+    defualt: 0.00
+  }
 }, {versionKey: false});
 
 const itemModel = mongoose.model('items', itemSchema);
