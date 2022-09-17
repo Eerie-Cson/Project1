06c6f742ec4b8d61bce0e646bdd1f1ded6bf99be
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-09-17 19:36:18 +0800

    refactor(api): destructurize the argument from the router (#36)
    * refactor(api): destructurize the argument from the router
    
    * style(api): remove unnecessary comment
    
    * style(api): remove unused variable

--- server/api/services/items/index.js
+++ server/api/services/items/index.js
@@ -1,16 +1,19 @@
 const items = require('./repository');
-const ID = require('../../../lib/generate-id');
 class ItemServices {
-  async createItem({item, stock, price}){
+  async createItem(data){
 
+    const {ID, input: {item, stock, price}} = data;
     const product = {
-      ID,
-      item,
-      stock,
-      price
+      id: ID,
+      data: {
+        item,
+        stock,
+        price
+      }
     } 
 
     return await items.create(product);
+
   };
 };
 
