ccef3de661de81116225b477644c8475d731de6a
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-09-18 17:37:15 +0800

    style(api): rename describe message to get model

--- server/api/tests/get-model.test.js
+++ server/api/tests/get-model.test.js
@@ -3,7 +3,7 @@ const itemModel = require('../model/item');
 
 const itemRepository = new ItemRepository();
 
-describe('CreateItem endpoint', () => {
+describe('Get model', () => {
   it('should get item model', () =>{
     itemRepository.setModel(itemModel);
     expect(itemRepository.getModel()).toBeDefined();
