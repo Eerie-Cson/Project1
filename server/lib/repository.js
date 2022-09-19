514b80c9d5f5f86968e0ef84a1d0bfc2947236ed
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-09-19 21:48:49 +0800

    feat(api): add find method in repository

--- server/lib/repository.js
+++ server/lib/repository.js
@@ -15,12 +15,15 @@ class Repository {
   }
 
   async create(params) {
-    
     const { data } = params;
     const result = await this.model.create({...data, id: params.id});
     return this.returnValue(result.toObject());
-
   };
+
+  async find(params) {
+    const result = await this.model.find(params);
+    return result;
+  }
 }
 
 module.exports = Repository;
