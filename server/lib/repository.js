9ec0e49f00534cf2c0e4ebb12b7e365bc7ead465
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-09-19 21:57:18 +0800

    Feature exists method (#45)
    * feat(api): add find method in repository
    
    * feat(api): add exists method in repository
    
    * style(api): add new line at eol

--- server/lib/repository.js
+++ server/lib/repository.js
@@ -15,12 +15,22 @@ class Repository {
   }
 
   async create(params) {
-    
     const { data } = params;
     const result = await this.model.create({...data, id: params.id});
     return this.returnValue(result.toObject());
-
   };
+
+  async exists(params) {
+    const found = await this.find(params);
+    if(found.length === 0 ) return false
+    return true 
+  }
+
+
+  async find(params) {
+    const result = await this.model.find(params);
+    return result;
+  }
 }
 
 module.exports = Repository;
