a3a79bb6efbd4ef805f2e1c926f857a146309b7f
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-09-19 22:29:27 +0800

    Feature exists integration test (#48)
    * refactor(api): modify the test input according to router changes
    
    * test(api): add integration test for exists function

--- server/api/tests/create-item.test.js
+++ server/api/tests/create-item.test.js
@@ -1,8 +1,14 @@
 const request = require('supertest'); 
-const app = require('../../server');
 const mongoose = require('mongoose');
 const itemModel = require('../model/item');
-const product = require('../../lib/generate-data')
+const { Chance } = require('chance');
+
+const ItemRepository = require('../../api/services/items/index');
+const app = require('../../server');
+const product = require('../../lib/generate-data');
+const { ID } = require('../../lib/generate-data');
+
+const itemRepository = new ItemRepository();
 
 beforeAll(async()=>{
   server = app.listen(3000);
@@ -22,12 +28,33 @@ afterAll(async () => {
 
 describe('CreateItem endpoint', () => {
   it('should create new item', async () => {
-
-    const {ID, ...input } = product;
-    const response = await request(server).post('/items').send(input);
-
+  
+    const {ID, ...data } = product;
+    const response = await request(server).post('/items').send(data);
+  
     expect(response.statusCode).toBe(200);
     expect(response.body.id).toBeDefined();
-    expect(response.body.data).toStrictEqual(input);
+    expect(response.body.data).toStrictEqual(data);
+  });
+  
+  it('should return error message', async() => {
+      
+    const chance = new Chance();
+    const {ID, ...data } = product;
+    
+    const input = {
+      ID: product.ID,
+      data: {
+        item: product.item,
+        stock: chance.natural({min: 1, max: 5000}),
+        price: chance.floating({min: 1, max: 5000, fixed: 2}),
+      }
+    };
+
+    await itemRepository.createItem(input);
+
+    const response = await request(server).post('/items').send(data);
+
+    expect(response.body).toStrictEqual({Error: 'Item already exist'});  
   })
 })
