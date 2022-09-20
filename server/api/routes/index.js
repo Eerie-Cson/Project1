aaabd5acebe90e0af80cdccd65f790ff50d041e8
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-09-20 20:42:07 +0800

    feat(api): add a get router for displaying all items

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -6,11 +6,15 @@ const chance = new Chance();
 const router = new Router();
 const items = new ItemServices();
 
-router.post('/items', async ctx => {
+router.post('/create/items', async ctx => {
   const ID = chance.string({length: 8, alpha: true, numeric: true, casing: 'upper'});
   ctx.body = await items.createItem({
     ID, 
     data: ctx.request.body
   });
+});
+
+router.get('/items', async ctx => {
+  ctx.body = await items.displayItems();
 })
 module.exports = router;
