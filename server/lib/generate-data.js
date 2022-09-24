26a8820d9de8311f09374fd3234d1cc7a14134d7
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-09-24 10:07:42 +0800

    refactor(api): create a function that can generate multiple product

--- server/lib/generate-data.js
+++ server/lib/generate-data.js
@@ -2,12 +2,18 @@ const { Chance } = require('chance');
 const R = require('ramda');
 
 const chance = new Chance();
-const product = {
-  ID: chance.string({length: 8, alpha: true, numeric: true, casing: 'upper'}),
-  item: chance.word(),
-  stock: chance.natural({min: 1, max: 5000}),
-  price: chance.floating({min: 1, max: 5000, fixed: 2}),
+const productData = () => {
+  const product = () => ({
+    ID: chance.string({length: 8, alpha: true, numeric: true, casing: 'upper'}),
+    item: chance.word(),
+    stock: chance.natural({min: 1, max: 5000}),
+    price: chance.floating({min: 1, max: 5000, fixed: 2}),
+  })
+  return {
+    times: R.times(product),
+    once:  () => product()
+  }
 };
 
-module.exports = product;
+module.exports = productData;
 
