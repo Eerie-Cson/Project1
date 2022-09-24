ffbde4c52fdfdae7b00ce80504d4b32ededbda07
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-09-24 10:24:22 +0800

    test(api): add unit test for creating multiple data

--- server/api/services/tests/index.test.js
+++ server/api/services/tests/index.test.js
@@ -3,7 +3,7 @@ const R = require('ramda');
 const ItemRepository = require('../items/repository')
 const product = require('../../../lib/generate-data')
 
-const items = new ItemServices();
+const itemServices = new ItemServices();
 
 afterEach(() => {
   jest.clearAllMocks();
@@ -12,25 +12,41 @@ afterEach(() => {
 describe('ItemServices', () => {
   describe('#createItem', () => {
     it('should create new item', async() => {
-      
-      const itemData = {
-        id: product.ID,
-        data: R.omit(['ID'])(product)
-      };
+
+      const item = product().once();
+      const input = R.omit(['ID'])(item);
 
       ItemRepository.exists = jest.fn();
-
       ItemRepository.create = jest.fn().mockReturnValue({
-        id: itemData.id,
-        data: itemData.input
+        id: item.ID,
+        data: input
       });
 
-      const result = await items.createItem(itemData);
+      const result = await itemServices.createItem({
+        ID: item.ID,
+        data: input
+      });
 
       expect(result).toBeDefined();
-      expect(result).toStrictEqual({id: itemData.id, data: itemData.input});
+      expect(result).toStrictEqual({id: item.ID, data: input});
       expect(ItemRepository.create).toBeCalled();
-      expect(ItemRepository.exists).toBeCalledTimes(1);
+      expect(ItemRepository.exists).toBeCalled();
+    });
+
+    it('should create multiple items', async () => {
+      const items = product().times(3);
+
+      ItemRepository.create = jest.fn();
+
+      await Promise.all([ 
+        items.map( (item) => itemServices.createItem({
+            ID: item.ID,
+            data: R.omit(['ID'])(item)
+        })),
+      ]);
+      
+      expect(ItemRepository.create).toHaveBeenCalledTimes(3)
     })
-  })
+  });
+ 
 })
